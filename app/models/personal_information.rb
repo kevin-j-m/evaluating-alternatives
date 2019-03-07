7927afeb5d1d5c77c1cb037486f3ca37aaa70907
Kevin Murphy <kevin@gnar.dog>
2019-03-06 22:09:10 -0500

    Warn user about editing personal information - unit test
    This also warns a user that editing their personal information may
    affect their eligibility. However, given the norm in the code base that
    flash messages are not verified via system tests due to the expense of
    running a system for such a trivial feature, that spec has been removed.
    
    Given the importance of the specifics of that message, in order to
    comply with FDA regulations, the `PersonalInformation` class will now
    respond with that message, which the controller will use to set the
    flash message. That method on the `PersonalInformation` class is tested
    instead, so there can be coverage that the message is as expected.

--- app/models/personal_information.rb
+++ app/models/personal_information.rb
@@ -3,11 +3,15 @@ class PersonalInformation
   delegate :first_name, to: :study_participant
   delegate :last_name, to: :study_participant
 
-  def initialize(study_participant_id)
+  def initialize(study_participant_id = nil)
     @study_participant_id = study_participant_id
   end
 
   def study_participant
     @study_participant ||= StudyParticipant.find(@study_participant_id)
+  end
+
+  def edit_warning_message
+    I18n.t("personal_information.edit.warning")
   end
 end
